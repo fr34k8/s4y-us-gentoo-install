f534bda81d959c00b0f2f0c73c96036214d93aab
@doerteDev (Stefan Staudenmeyer) <stefan.staudenmeyer@csiweb.de>
2014-04-07 18:36:01 +0200

    FIRST BLOOD!

---
+++ setup.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+DISKS="/dev/sda /dev/sdb"
+RAID_BOOT=""
+RAID_SYSTEM=""
+
+# Test for tools
+TOOLS="sgdisk mdadm openssl cryptsetup pvcreate vgcreate lvcreate mkfs.ext4 mkswap mount wget chroot"
+for tool in $TOOLS; do
+	available=`which $tool`
+	if [ "x${sgdisk}" = "x" ]; then
+		echo "necessary tool $tool not found! INSTALL!"
+		exit 1
+	fi
+done
+
+sgdisk=`which sgdisk`
+mdadm=`which mdadm`
+openssl=`which openssl`
+cryptsetup=`which cryptsetup`
+pvcreate=`which pvcreate`
+vgcreate=`which vgcreate`
+lvcreate=`which lvcreate`
+mkfs=`which mkfs.ext4`
+mkswap=`which mkswap`
+mkdir=`which mkdir`
+mount=`which mount`
+wget=`which wget`
+chroot=`which chroot`
+
+boot_parts=""
+system_parts=""
+
+# partition disks
+disknr=0
+for disk in $DISKS; do
+
+	partitionnr=1
+
+	$sgdisk --clear $disk
+
+	$sgdisk --new=$partitionnr:0:+2M $disk
+
+	$sgdisk --typecode=$partitionnr:ef02 $disk
+
+	(( partitionnr++ ))
+
+	$sgdisk --new=$partitionnr:0:+100M $disk
+
+	$sgdisk --typecode=$partitionnr:fd00 $disk
+
+	boot_parts="${boot_parts} ${disk}${partitionnr}"
+
+	(( partitionnr++ ))
+
+	$sgdisk --largest-new=$partitionnr $disk
+
+	$sgdisk --typecode=$partitionnr:fd00 $disk
+
+	system_parts="${system_parts} ${disk}${partitionnr}"
+
+	echo "disk layout for ${disk}:"
+	$sgdisk --print $disk
+
+	(( disknr++ ))
+done
+
+echo "partitions for boot raid: ${boot_parts}"
+echo "partitions for root raid: ${system_parts}"
+
+# Create raids
+raidnr=0
+boot_raid=""
+while [[ -z $boot_raid  ]]; do
+	if [ ! -e "/dev/md${raidnr}" ]; then
+		boot_raid="/dev/md${raidnr}"
+	fi
+	(( raidnr++ ))
+	if (( r > 127 )); then
+		echo "no raid device found!"
+		exit 1;
+	fi
+done
+$mdadm --create $boot_raid --raid-devices=2 --level=1 --metadata=0.9 $boot_parts
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+system_raid=""
+while [[ -z $system_raid  ]]; do
+	if [ ! -e "/dev/md${raidnr}" ]; then
+		system_raid="/dev/md${raidnr}"
+	fi
+	(( raidnr++ ))
+	if (( raidnr > 127 )); then
+		echo "no raid device found!"
+		exit 1;
+	fi
+done
+$mdadm --create $system_raid --raid-devices=2 --level=1 $system_parts
+
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+# Generate keyfile for system LUKS
+$openssl rand -base64 48 >keyfile
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+# Setup luks
+$cryptsetup --cipher=aes-xts-plain64 --key-size=512 --hash=sha512  --iter-time=5000 --use-random --batch-mode --key-file=./keyfile luksFormat $system_raid
+if [ $? != 0 ]; then
+	exit 1
+fi
+$cryptsetup --key-file=keyfile luksOpen $system_raid system
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+system_luks="/dev/mapper/system"
+
+# Setup LVM
+$pvcreate $system_luks
+
+system_vg="vg0"
+$vgcreate $system_vg $system_luks
+
+$lvcreate --name=swap --size=4G $system_vg
+$lvcreate --name=root --size=50G $system_vg
+$lvcreate --name=home --extents=100%FREE $system_vg
+lv_swap="/dev/mapper/${system_vg}-swap"
+lv_root="/dev/mapper/${system_vg}-root"
+lv_home="/dev/mapper/${system_vg}-home"
+
+# Setup filesystems
+$mkfs -L boot $boot_raid
+$mkfs -L root $lv_root
+$mkfs -L home $lv_home
+$mkswap -L swap $lv_swap
+
+# Mount devices
+$mkdir -p /mnt/gentoo
+
+$mount $lv_root /mnt/gentoo
+$mkdir -p /mnt/gentoo/{boot,home}
+
+$mount $boot_raid /mnt/gentoo/boot
+$mount $lv_home /mnt/gentoo/home
+
+mv keyfile /mnt/gentoo/boot/keyfile
+
+# Get stage3
+cd /mnt/gentoo
+$wget http://gentoo.mirrors.pair.com/releases/amd64/current-iso/stage3-amd64-20140403.tar.bz2
+tar xjpf $stage3
+
+# Get gentoo install script
+$wget XXXCHROOTSCRIPTXXX --output-document=XXXSCRIPTPATHXXX
+chmod +x root/XXXSCRIPTPATHXXX
+
+# Prepare chroot
+$mount -t proc none /mnt/gentoo/proc
+$mount --rbind /dev /mnt/gentoo/dev
+$mount --rbind /sys /mnt/gentoo/sys
+cp -L /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
+
+# Export vars for chroot
+echo "boot_raid=\"${boot_raid}\"
+system_raid=\"${system_raid}\"
+lv_root=\"${lv_root}\"
+lv_home=\"${lv_home}\"
+lv_swap=\"${lv_swap}\"" >/mnt/gentoo/root/XXXSCRIPTPATHXXX
+
+# chroot
+$chroot /mnt/gentoo /root/XXXSCRIPTPATHXXX
---
+++ setup_postchroot.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+mkdir -p /usr/portage
+mkdir -p /usr/local/portage
+
+source /etc/profile
+env-update
+export PS1="(chroot) $PS1"
+
+# Prepare make.conf
+wget XXXMAKE.CONFXXX --output-document=/etc/portage/make.conf
+
+# Create Portage dirs
+mkdir -p /etc/portage/{package.use,package.keywords,package.mask,package.unmask,package.env}
+
+# sync portage tree
+emerge-webrsync
+emerge --sync
+
+# select normal profile
+eselect profile set default/linux/amd64/13.0
+KERNEL="sys-kernel/gentoo-sources"
+
+# set timezone
+echo "UTC" > /etc/timezone
+emerge --config sys-libs/timezone-data
+
+# set locales
+echo "en_US ISO-8859-1
+en_US.UTF-8 UTF-8
+de_DE ISO-8859-1
+de_DE.UTF-8 UTF-8
+de_DE@euro ISO-8859-15" >>/etc/locale.gen
+
+locale-gen
+
+eselect locale set en_US.UTF-8
+
+echo 'LANG="en_US.UTF-8"
+LC_COLLATE="C"' >/etc/env.d/02locale
+
+# reload shell
+env-update
+source /etc/profile
+export PS1="(chroot) $PS1"
+
+# unmask kernel and genkernel and install it
+echo "${KERNEL} ~amd64" >/etc/portage/package.keywords/sys-kernel
+echo "sys-kernel/genkernel-next ~amd64" >>/etc/portage/package.keywords/sys-kernel
+echo "sys-kernel/genkernel-next cryptsetup" >>/etc/portage/package.use/sys-kernel
+
+emerge ${KERNEL} sys-kernel/genkernel-next mdadm cryptsetup lvm2
+cd /usr/src/linux
+
+# Get kernel config
+wget XXXKERNEL.CONFXXX --output-document=.config
+
+# make and install
+make -j9
+make install modules_install
+
+cd /root
+
+# edit genkernel config
+sed -i 's/#LVM="no"/LVM="yes"/' /etc/genkernel.conf
+sed -i 's/#LUKS="no"/LUKS="yes"/' /etc/genkernel.conf
+sed -i 's/#BUSYBOX="no"/BUSYBOX="yes"/' /etc/genkernel.conf
+sed -i 's/#UDEV="no"/UDEV="yes"/' /etc/genkernel.conf
+sed -i 's/#MDADM="no"/MDADM="yes"/' /etc/genkernel.conf
+sed -i 's/#E2FSPROGS="no"/E2FSPROGS="yes"/' /etc/genkernel.conf
+sed -i 's!#REAL_ROOT="/dev/one/two/gentoo"!REAL_ROOT="/dev/mapper/${lv_root}"!' /etc/genkernel.conf
+
+# generate initramfs
+genkernel initramfs
+
+# Load vars from setup script
+source doertedev_gentoo.vars
+
+# get UUIDs from device names
+uuid_boot=`blkid | grep ${boot_raid} | grep -Po '\bUUID="([^"]+)"'`
+uuid_system=`blkid | grep  ${system_raid} | grep -Po '\bUUID="([^"]+)"'`
+uuid_root=`blkid | grep  ${lv_root} | grep -Po '\bUUID="([^"]+)"'`
+uuid_var=`blkid | grep ${lv_var} | grep -Po '\bUUID="([^"]+)"'`
+uuid_storage=`blkid | grep  ${lv_storage} | grep -Po '\bUUID="([^"]+)"'`
+uuid_swap=`blkid | grep  ${lv_swap} | grep -Po '\bUUID="([^"]+)"'`
+
+# fstab
+echo "${uuid_boot}       /boot             ext4        relatime    1 2" >>/etc/fstab
+echo "${uuid_root}       /                 ext4        relatime    0 1" >>/etc/fstab
+echo "${uuid_var}        /var             ext4        relatime    0 3" >>/etc/fstab
+echo "${uuid_storage}    /mnt/storage     ext4        relatime    0 4" >>/etc/fstab
+echo "${uuid_swap}       none             swap        sw            0 0" >>/etc/fstab
+echo "none               /tmp            tmpfs        rw,size=8G,nodev,noatime    0 0" >>/etc/fstab
+
+# Set hostname
+echo "hostname=\"XXXURHOSTNAMEXXX\"" >/etc/conf.d/hostname
+
+# Network
+IFACE=`udevadm test-builtin net_id /sys/class/net/eth0 2>/dev/null | grep ID_NET_NAME_PATH | awk -F= '{print $2}'`
+
+echo "modules=( \"iproute2\" )" >/etc/conf.d/net
+echo "" >>/etc/conf.d/net
+echo "config_${IFACE}=\"XXXURIPXXX/24\"" >>/etc/conf.d/net
+echo "routes_${IFACE}=(" >>/etc/conf.d/net
+echo "    \"XXXURROUTEXXX dev ${IFACE}\"" >>/etc/conf.d/net
+echo "    \"default via XXXURROUTEXXX\"" >>/etc/conf.d/net
+echo ")" >>/etc/conf.d/net
+
+cd /etc/init.d
+ln -s net.lo net.${IFACE}
+rc-update add net.${IFACE} default
+cd /root
+
+# Set SSH key
+mkdir .ssh
+echo "ssh-ed25519 XXXURKEYXXX" >.ssh/authorized_keys
+
+# Install system tools
+emerge syslog-ng sys-process/cronie logrotate iproute2
+rc-update add syslog-ng default
+rc-update add cronie default
+rc-update add sshd default
+rc-update add lvm default
+rc-update add mdadm default
+
+# Install bootloader
+emerge sys-boot/grub
+echo "GRUB_CMDLINE_LINUX=\"rootfstype=ext4 domdadm dolvm crypt_roots=${uuid_system} root_key=keyfile root_keydev=${uuid_boot}\"" >>/etc/default/grub
+echo "GRUB_PRELOAD_MODULES=lvm" >>/etc/default/grub
+
+grub2-install --recheck /dev/sda
+grub2-mkconfig -o /boot/grub/grub.cfg
+
+exit
